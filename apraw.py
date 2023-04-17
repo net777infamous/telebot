6bbe1b2905ccf1f7615c27f620decd8d4b4e00fe
net777infamous <43906637+net777infamous@users.noreply.github.com>
2023-04-17 18:10:59 +0200

    Update apraw.py

--- apraw.py
+++ apraw.py
@@ -66,7 +66,7 @@ reddit = praw.Reddit(
     password="drahciR0nline",
     user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
 )
-openai.api_key = "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"
+openai.api_key = "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"
 # Specify the subreddit you want to get posts from
 
 # Define the function that will handle the start command
